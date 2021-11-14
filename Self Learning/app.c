4c7f90d8a9af22196d9b170684e9c577b54c11c6
Eyal hagai <eyalh2001@gmail.com>
2021-11-14 12:42:27 +0200

    playing with malloc

--- Self Learning/app.c
+++ Self Learning/app.c
@@ -1,41 +1,26 @@
 #include <stdio.h>
-
-void swap(char arr[], int a, int b);
-int minIndex(char arr[], int size, int start);
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-    char arr[] = "gajbhuytrewqzxcfdpolkmnsiv";
-    int size = sizeof(arr) - 1;
+    printf("Enter array size: ");
+    size_t size;
+    scanf("%ld", &size);
+    printf("\narray size: %ld\n", size);
+
+    int* array = (int*)malloc(size);
 
     for (int i = 0; i < size; ++i){
-        int index = minIndex(arr, size, i);
-        swap(arr, i, index);
+        array[i] = i;
     }
 
     for (int i = 0; i < size; ++i){
-        printf("%c", arr[i]);
+        printf("%d, ", array[i]);
     }
+
     printf("\n");
 
-    return 0;
 }
 
-void swap(char arr[], int a, int b){
-    char temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
-}
 
-int minIndex(char arr[], int size, int start){
-    char min = arr[start];
-    int index = start;
-    for (int i = start + 1; i < size; ++i){
-        if (arr[i] < min){
-            min = arr[i];
-            index = i;
-        }
-    }
 
-    return index;
-}
